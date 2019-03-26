8b0caed8ed5c2766dced7bde308a7b38f4b0aed0
Irakli <iraklikardawa@gmail.com>
2019-03-26 13:05:18 +0100

    new entities added

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,26 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [
         RouterModule.forChild([
+            {
+                path: 'przedmiot',
+                loadChildren: './przedmiot/przedmiot.module#OnlineschoolPrzedmiotModule'
+            },
+            {
+                path: 'klasa',
+                loadChildren: './klasa/klasa.module#OnlineschoolKlasaModule'
+            },
+            {
+                path: 'ocena',
+                loadChildren: './ocena/ocena.module#OnlineschoolOcenaModule'
+            },
+            {
+                path: 'nauczyciel',
+                loadChildren: './nauczyciel/nauczyciel.module#OnlineschoolNauczycielModule'
+            },
+            {
+                path: 'uczen',
+                loadChildren: './uczen/uczen.module#OnlineschoolUczenModule'
+            }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
     ],
